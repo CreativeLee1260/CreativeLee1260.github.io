64eabaf1705ab18c1948f103584012759153f335
CreativeLee1260 <leesebastian.campos@stu.cvccworks.edu>
2024-10-24 13:17:33 -0400

    Add files via upload

---
+++ works3/apply-ch10/scripts/script.js
@@ -0,0 +1,14 @@
+/*
+Name: Lee Campos
+File Name: script.js
+Date: 10/22/24
+*/
+
+
+function content()
+{
+    var text = document.getElementById("new");
+    text.textContent = "Hi. I am a new paragraph that was created with a JavaScript function.";
+    text.style.color = "#c0145f";
+    text.style.fontSize = "2em";
+}
